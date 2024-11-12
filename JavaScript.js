38595a6dc52fc3c03b9fa2f79274df21874b6c69
QXC8 <142752125+QXC8@users.noreply.github.com>
2024-11-12 15:15:42 -0300

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,27 @@
+const username = document.getElementById("username")
+const password = document.getElementById("password")
+const button = document.getElementById("button")
+
+const CorrectUsername = "Admin"
+const CorrectPassword = "123"
+
+button.addEventListener("click", (e) => {
+    e.preventDefault()
+    const data = {
+        username: username.value,
+        password: password.value 
+    }
+    console.log(data)
+})
+
+button.addEventListener("click", (e) => {
+
+if (username.value === CorrectUsername && password.value === CorrectPassword) {
+    window.location.href = "css3/css3.html";
+} else {
+    alert("Usuario o contraseña incorrectos");
+}
+
+});
+
+
